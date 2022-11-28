31a26d7d771319bf2a4237d7ba03a4f86e135b0e
Avinash Mahala <avinash.mahala.am@outlook.com>
2022-11-28 03:32:46 -0600

    update png file

--- in.asm
+++ in.asm
@@ -1,9 +1,9 @@
-; Adding two 1 x N vectors
+; A small (CAT) program to add two 1 x N vectors (that is add the corresponding values in two lists of numbers),
         go   0
 0       ld   2	.count
-	ldi  3	.list1
-        ldi  4	.list2
-        ldi  5	.result
+		ldi  3	.numList1
+        ldi  4	.numList2
+        ldi  5	.numRes
 .loop	ld   1  *3
         add  1  *4
         st   1  *5
@@ -15,14 +15,13 @@
         bnz  2  .loop
         sys  1  18
 .count  dw   3
-.list1  dw   1
+.numList1  dw   7
+        dw   1
         dw   2
-        dw   3
-.list2  dw   4
-        dw   5
-        dw   6
-.result dw   0
+.numList2  dw   1
+        dw   2
+        dw   4
+.numRes  dw   0
         dw   0
         dw   0
-56      dw   0
         end
